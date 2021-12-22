60347c1cb2971cdcabb1cb8273d159940f408617
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2021-12-22 16:02:31 -0300

    aula 2: sintaxe basica js

---
+++ Exercícios/Sintaxe Básica de Javascript/Aula 2 - Entendendo variáveis/script.js
@@ -0,0 +1,24 @@
+//tipos primitivos
+
+//boolean
+var vf = true;
+console.log(typeof(vf) + ': ' + vf);
+
+//numero
+var numero = 3;
+console.log(typeof(numero) + ': ' + numero);
+
+// string
+var texto = "tudo bem?";
+console.log(typeof(texto) + ': ' + texto);
+
+var indefinida;
+console.log(typeof(indefinida) + ": " + indefinida);
+indefinida = "definida!"
+console.log(typeof(indefinida) + ": " + indefinida);
+
+let variavelDefinida = 10;
+console.log(typeof(variavelDefinida) + ': ' + variavelDefinida);
+
+const constante = 'sou constante!';
+console.log(typeof(constante) + ': ' + constante);
