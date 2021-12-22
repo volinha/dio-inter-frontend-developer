79eb8920e7a8db14e1987be8671cbdf5bf6dcfdb
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2021-12-22 11:55:24 -0300

    js: addEventListener + condicionais + cor do texto

--- Exercícios/Introdução ao Javascript/web/assets/js/scripts.js
+++ Exercícios/Introdução ao Javascript/web/assets/js/scripts.js
@@ -1,12 +1,30 @@
-var numeroAtualWrapper = document.getElementById('currentNumber');
-var numeroAtual = 0;
+var btnAdicionar = document.getElementById('adicionar');            //elemento do botão de adicionar
+var btnSubtrair = document.getElementById('subtrair');              //elemento do botão de subtrair
+var botao = document.getElementsByClassName('botao');
 
-function incrementa() {
-    numeroAtual = numeroAtual + 1;
-    numeroAtualWrapper.innerHTML = numeroAtual;
-}
+var numeroWrapper = document.getElementById('currentNumber');       //wrapper do span com o número
+var numero = 0;                                                     //valor do número
 
-function decrementa() {
-    numeroAtual = numeroAtual - 1;
-    numeroAtualWrapper.innerHTML = numeroAtual;
-}
+btnAdicionar.addEventListener('click', function () {                //listener para clicks no botão de adicionar
+    if(numero < 10){                                                //limita a 10
+        numero = numero + 1;
+        numeroWrapper.innerHTML = numero;
+    }
+    // poderia ser feito desabilitando o botão com btnAdicionar.disabled = true, mas esta é uma solução mais simples
+    
+    if(numero >= 0){
+        numeroWrapper.style.color = 'black';
+    }
+    // se >= 0, altera a cor para a padrão
+});
+
+btnSubtrair.addEventListener('click', function () {                 //listener para clicks no botão de subtrair
+    if(numero > - 10){                                              //limita a -10
+        numero = numero - 1;
+        numeroWrapper.innerHTML = numero;
+    }
+    if(numero < 0){
+        numeroWrapper.style.color = 'red';
+    }
+    // se < 0, altera a cor para vermelho
+});
