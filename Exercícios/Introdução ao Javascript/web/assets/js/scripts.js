ef69fc0a0e817ffbd968619574b338d5cca9a913
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2021-12-22 11:14:32 -0300

    js: contador

--- Exercícios/Introdução ao Javascript/web/assets/js/scripts.js
+++ Exercícios/Introdução ao Javascript/web/assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log("Olá mundo!");
+var numeroAtualWrapper = document.getElementById('currentNumber');
+var numeroAtual = 0;
+
+function incrementa() {
+    numeroAtual = numeroAtual + 1;
+    numeroAtualWrapper.innerHTML = numeroAtual;
+}
+
+function decrementa() {
+    numeroAtual = numeroAtual - 1;
+    numeroAtualWrapper.innerHTML = numeroAtual;
+}
